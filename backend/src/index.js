7346576d9e5040a8c1b671c271765d103fcd42d6
Agustin Palmieri <agujex@Agustins-MBP.utopia.net>
2020-01-08 12:50:06 -0300

    init test server with async await

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,3 +2,10 @@ const app = require('./app');
 
 
 app.listen(4000, ()=> console.log('on port 4000'))
+
+async function main() {
+    await app.listen(4000);
+    console.log('Server on port 4000');
+}
+
+main()
