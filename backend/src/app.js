89f37fd3af4314431ae5f157348102923f9c7030
Agustin Palmieri <agujex@Agustins-MBP.utopia.net>
2020-01-09 08:06:42 -0300

    add port in .env & add info in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,23 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
+
+// settings
+
+app.set('port', process.env.PORT || 3000);   // send data to frontend - from app set the port('port' is a variable - 4000 is the value)
+                                        // If process.env.PORT exist, catch it(.env). If not use 4000.
+
+
+
+
+// middlewares
+app.use(cors());
+app.use(express.json()); //send json files
+
+// routes
+
+app.get('/users', (req, res) => res.send('users routes'))
+app.get('/notes', (req, res) => res.send('notes routes'))
 
 module.exports = app;
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,8 @@ require('./database');
 
 
 async function main() {
-    await app.listen(4000);
-    console.log('Server on port 4000');
+    await app.listen(app.get('port'));
+    console.log('Server on port', app.get('port'));
 }
 
 main()
