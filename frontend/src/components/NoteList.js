185824a2e9cb650d726d96eb7280f7cdfca59462
Agustin Palmieri <agujex@Agustins-MBP.utopia.net>
2020-01-09 20:44:41 -0300

    Create components and use Router in App.js

---
+++ frontend/src/components/NoteList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NoteList extends Component {
+    render() {
+        return (
+            <div>
+                Note list
+            </div>
+        )
+    }
+}
